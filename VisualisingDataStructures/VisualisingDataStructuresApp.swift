364c35d7692cb2366986e11526f916ba7edb8843
Ssnehnil Ghosh <ghosh.s@etoncollege.org.uk>
2022-03-03 11:53:29 +0000

    Initial Commit

---
+++ VisualisingDataStructures/VisualisingDataStructuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataStructuresApp.swift
+//  VisualisingDataStructures
+//
+//  Created by Ghosh, Ssnehnil (BJH) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataStructuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
